60b23cbe77a71940b836c365d82b55704676f872
makedo <krivousa@gmail.com>
2019-09-01 20:01:11 +0300

    Renamed Page builder pipe spec

--- spec/Paginator/Page/Builder/HasNextByPagesCountSpec.php
+++ spec/Paginator/Page/Builder/HasNextByPagesCountSpec.php
@@ -2,22 +2,23 @@
 
 namespace spec\Makedo\Paginator\Page\Builder;
 
-use Makedo\Paginator\Page\Builder\HasNextByPages;
+use Makedo\Paginator\Page\Builder\HasNextByPagesCount;
 use Makedo\Paginator\Page\Builder\Pipe;
 use Makedo\Paginator\Page\Page;
 use PhpSpec\ObjectBehavior;
+use PhpSpec\Wrapper\Subject;
 
-class HasNextByPagesSpec extends ObjectBehavior
+class HasNextByPagesCountSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
-        $this->shouldHaveType(HasNextByPages::class);
+        $this->shouldHaveType(HasNextByPagesCount::class);
         $this->shouldImplement(Pipe::class);
     }
 
     function it_fills_page_with_has_next_value_true(Page $page)
     {
-        /** @var Mock $page */
+        /** @var Page|Subject $page */
         $page = $page->getWrappedObject();
         $page->totalPages = 10;
         $page->currentPage = 2;
@@ -29,6 +30,7 @@ class HasNextByPagesSpec extends ObjectBehavior
 
     function it_fills_page_with_has_next_value_false(Page $page)
     {
+        /** @var Page|Subject $page */
         $page = $page->getWrappedObject();
         $page->totalPages = 10;
         $page->currentPage = 10;
